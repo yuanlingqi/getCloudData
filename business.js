ac1d5f7e7679f43c2568e7a2e77f52f4a08ab03d
Terence Zhang <Terence.Zhang@arm.com>
2018-09-04 09:50:55 +0800

    Update for subscribe Resource Example: * Use subscribe function to replace checkdevice. * Use 4 space to replace tab.

--- business.js
+++ business.js
@@ -2,12 +2,10 @@ require('dotenv').config({ silent: true });
 var logger = require('./logger');
 var request = require('./httpRequest');
 
-
 var deviceId = "0164ea12f06e00000000000100100225";
-
 //电表
 var resourcePaths = ["/20003/1/26267", "/20003/1/26266", "/20003/1/26259", "/20003/1/26263", "/20003/1/26255", "/20003/1/26251", "/20003/1/26247", "/20003/1/26241", "/20003/1/30008", "/20003/1/30007", "/20003/1/30006", "/20003/1/30005", "/20003/1/30004", "/20003/1/30003"
-					,"/20002/1/28004", "/20002/1/31002", "/20002/1/31003", "/20002/1/31004", "/20002/1/31005", "/20002/1/31006", "/20002/1/31007","/20002/3/31008"];
+                ,"/20002/1/28004", "/20002/1/31002", "/20002/1/31003", "/20002/1/31004", "/20002/1/31005", "/20002/1/31006", "/20002/1/31007","/20002/3/31008"];
 
 //控制断路器分合闸的DO模块
 // var resourcePaths2 = ["/20002/1/28004", "/20002/1/31002", "/20002/1/31003", "/20002/1/31004", "/20002/1/31005", "/20002/1/31006", "/20002/1/31007"];
@@ -18,36 +16,38 @@ var connect = new MbedCloudSDK.ConnectApi(config);
 
 //获取连接设备列表
 connect.listConnectedDevices(function(error, response) {
+	console.log("List Connected Devices - >");
     if (error){
-    	logger.error(error.message);
-    	return;
+        logger.error(error.message);
+        return;
     }
 
     if (response.length == 0)
     	logger.error("No Devices Found"); 
     else 
-    	checkDevices(response.data);
+        //checkDevices(response.data);
+        /*No need to check device but just subscribe the relevant resouces */
+        subscribeRes();
 });
 
 
 //遍历设备列表检查目标设备是否存在
 function checkDevices(devices){
-	logger.info("devices.length " + devices.length);
-	for (var i = devices.length - 1; i >= 0; i--) {
-		var device = devices[i];
-		if(device.id == deviceId){
-			logger.info("deviceId: " + device.id + " exists.");
-			// subscribe to two paths on two specific device
-			connect.subscribe.resourceValues({ deviceId: [deviceId], resourcePaths: resourcePaths})
-		                                      .addListener(res => logger.info(res));
-		}
-	}
+    logger.info("devices.length " + devices.length);
+    for (var i = devices.length - 1; i >= 0; i--) {
+        var device = devices[i];
+        if(device.id == deviceId){
+            logger.info("deviceId: " + device.id + " exists.");
+            // subscribe to two paths on two specific device
+            connect.subscribe.resourceValues({ deviceId: [deviceId], resourcePaths: resourcePaths})
+                                                .addListener(res => logger.info(res));
+        }
+    }
 }
 
-
-
-                              
-
-
-
-
+/*Subscribe all relevant resouces from all devices */
+function subscribeRes(){
+    console.log("Subscribe the Resource - >");
+    connect.subscribe.resourceValues({resourcePaths: resourcePaths}, "OnValueUpdate")
+            .addListener(res => console.log(res));
+}

816b18a6f98117b38e94a1a7c8d5da25a003652a
Terence Zhang <Terence.Zhang@arm.com>
2018-09-13 07:49:54 +0800

    Example for Chint Demo: * Use arm settings as default, need to change all places with "ARM-CHINT" comments * Add recovery function via heartbeats * Add webhook which is recommend, run webapp on AWS instance

--- business.js
+++ business.js
@@ -2,8 +2,17 @@ require('dotenv').config({ silent: true });
 var logger = require('./logger');
 var request = require('./httpRequest');
 var http = require('http');
+var MbedCloudSDK = require("mbed-cloud-sdk");
+/*******************************************************************************************
+ * DEVICE AND RESOURCE
+********************************************************************************************/
 var deviceId = "0164ea12f06e00000000000100100225";
 // var deviceId = "0165b275783a0000000000010010008e"
+
+/* heartbeat resource used for watchdog - ARM-CHINT*/
+var heartbeatRes = ["/3200/0/5501"];
+//var heartbeatRes =["/20002/3/31008"];
+
 //电表
 var resourcePaths = ["/20003/1/26267", "/20003/1/26266", "/20003/1/26259", "/20003/1/26263", "/20003/1/26255", "/20003/1/26251", "/20003/1/26247", "/20003/1/26241", "/20003/1/30008", "/20003/1/30007", "/20003/1/30006", "/20003/1/30005", "/20003/1/30004", "/20003/1/30003"
                 ,"/20002/1/28004", "/20002/1/31002", "/20002/1/31003", "/20002/1/31004", "/20002/1/31005", "/20002/1/31006", "/20002/1/31007","/20002/3/31008", "/20003/4/26241"];
@@ -11,15 +20,11 @@ var resourcePaths = ["/20003/1/26267", "/20003/1/26266", "/20003/1/26259", "/200
 //控制断路器分合闸的DO模块
 // var resourcePaths2 = ["/20002/1/28004", "/20002/1/31002", "/20002/1/31003", "/20002/1/31004", "/20002/1/31005", "/20002/1/31006", "/20002/1/31007"];
 
-
-
-
-
-
+/*******************************************************************************************
+ * EXCEPTION RECOVERY - TBD
+********************************************************************************************/
 //异常恢复
 var httpStatus = 1;
-var preHeartBeat;
-var curHeartBeat;
 var timer;
 var duration = process.env.SCHEDULE_DURATION || 60;
 var exceptionTimes = 0;
@@ -73,50 +78,144 @@ function startSchedule(){
   cache = null; // Enable garbage collection
   logger.info('timerId:----------' + sid);
 }
+/*******************************************************************************************
+ * MBED CLOUD SDK INIT
+********************************************************************************************/
+var ApiKey_ArmTZ = "ak_1MDE1ODc3OTAzOGI0MDI0MjBhMDE0YzExMDAwMDAwMDA015d725ade7e02420a010d0600000000aGO6EmcXj7VWBymaYUBQCGfdM62CChWw";
+var ApiKey_Chint = "ak_1MDE2MjI5NGJkOTU3MGE1ODBhMDEyNDI4MDAwMDAwMDA016541f550d08669b5e804f500000000GOAaqJeIacOJuYQXCkKIN3OujXenUHmR";
+var Host_ArmUS  = "https://api.us-east-1.mbedcloud.com"
 
-var MbedCloudSDK = require("./node_modules/mbed-cloud-sdk/index");
-var config = require("./node_modules/mbed-cloud-sdk/examples/node/config");
+/* CHINT-ARM */
+//var accessKey = process.env.MBED_CLOUD_API_KEY || ApiKey_Chint;
+var accessKey = process.env.MBED_CLOUD_API_KEY || ApiKey_ArmTZ;
+var apiHost = process.env.MBED_CLOUD_HOST || Host_ArmUS;
+
+var config = {
+	apiKey: accessKey,
+	host: apiHost
+};
+
 var connect = new MbedCloudSDK.ConnectApi(config);
 
-connectDevices();
-
-//获取连接设备列表
-function connectDevices(){
-    connect.listConnectedDevices(function(error, response) {
-        console.log("List Connected Devices - >");
-        if (error){
-            logger.error(error.message);
-            return;
-        }
-
-        if (response.length == 0)
-            logger.error("No Devices Found"); 
-        else {
-            /*No need to check device but just subscribe the relevant resouces */
-            subscribeRes();
-        }
+/*******************************************************************************************
+ * FUNTIONS
+********************************************************************************************/
+// List Connected Devices
+function listDevices() {
+    console.log("----List Connected Devices ---- >");
+    return connect.listConnectedDevices()
+    .then(response => {
+        //console.log(response);
+        response.data.forEach(device => {
+            console.log(`Device: ${device.id}`);
+        });
+        
+    })
+    .catch(error => {
+        console.log(`\t└\x1b[0m: Error: ${error.message}`);
     });
 }
 
 /*Subscribe all relevant resouces from all devices */
 function subscribeRes(){
-    console.log("Subscribe the Resource - >");
-    connect.removeAllListeners("OnValueUpdate");
-    connect.deleteDeviceSubscriptions(deviceId).catch(
-        error => {
+    console.log("----Subscribe the resources ---->");
+    /*Subscribe the resources */
+    connect.subscribe.resourceValues({resourcePaths: resourcePaths}, "OnValueUpdate")
+        .addListener(res => {
+                console.log(res);
+                logger.info(res);
+            })
+        .addLocalFilter(res => res.contentType != undefined);
+
+    /*Subscribe the heartbeat resource */
+    connect.subscribe.resourceValues({ resourcePaths: heartbeatRes })
+        .addListener(res => {
+            console.log('heartbeatRes:' + res.payload);
+            curHeartBeat = res.payload;
+        });
+}
+
+function recoverSubscribeRes(){
+    /* Stop the Interval */
+    clearInterval(watchdogInterval);
+
+    /* Remove all subscriptions and presubscriptions */
+    connect.deleteSubscriptions()
+    .catch(error => {
         console.log(error);
     });
-    connect.subscribe.resourceValues({resourcePaths: resourcePaths}, "OnValueUpdate")
-            .addListener(res => 
-                {
-                    logger.info(res);
-                    if (res.path == '/20002/3/31008') {
-                        curHeartBeat = res.payload;
-                    }
-                })
-            .addLocalFilter(res => res.contentType != undefined);
-    clearInterval(timer);
-    startSchedule();
+    connect.deletePresubscriptions()
+    .catch(error => {
+        console.log(error);
+    });
+
+    /* Redo the subscription and watchdog  */
+    subscribeRes();
+    startWatchDog();
 }
 
 
+function checkHeartbeat(){
+    console.log('[checkHeartbeat]' + 'curHeartBeat=' + curHeartBeat + ' preHeartBeat=' + preHeartBeat);
+    if(curHeartBeat == preHeartBeat){
+        console.log('[checkHeartbeat] The heartbeat stopped for ' + timeoutHeartbeat + 'seconds, need to reset!!');
+        recoverSubscribeRes();
+    }else{
+        preHeartBeat = curHeartBeat;
+    }
+
+}
+
+/* Check heartbeat resource every "timeout_heartbeat", if the value did not changed, recover*/
+var timeoutHeartbeat = 35;
+/* Watchdog for Subscription */
+var watchdogInterval;
+
+function startWatchDog(){
+    curHeartBeat = 0;
+    preHeartBeat = 0;
+    watchdogInterval = setInterval(function(){checkHeartbeat()}, timeoutHeartbeat*1000);
+}
+
+/*******************************************************************************************
+ * APP START
+********************************************************************************************/
+function mainApp(){
+    /*Just print out all connected devices, not really need */
+    listDevices();
+    /*Subscribe all resources need to monitor  */
+    subscribeRes();
+    /*Start the watchdog to handle hungup issues */
+    startWatchDog();
+}
+
+/* No webhook, run on local machine ARM-CHINT */
+mainApp();
+
+/* Webhook, run on AWS instance ARM-CHINT*/
+/** The url is the full url address of server */
+/*
+var url = "http://2976cb8f.ngrok.io";
+connect.getWebhook()
+    .then(webhook => {
+        if (webhook) {
+            if (webhook.url === url) {
+                console.log(`Webhook already set to ${url}`);
+                return;
+            } else {
+                console.log(`Webhook currently set to ${webhook.url}, changing to ${url}`);
+            }
+        } else {
+            console.log(`No webhook currently registered, setting to ${url}`);
+        }
+
+        return connect.updateWebhook(url);
+    })
+    .then(() => {
+        mainApp();
+    })
+    .catch(error => {
+        console.log(`${error.message} - Unable to set webhook to ${url}, please ensure the URL is publicly accessible`);
+        process.exit();
+    });
+*/
